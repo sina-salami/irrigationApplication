db3f09bdffe29963b234597444fde7b7410d7127
Farhad Beigirad <f.beigirad@gmail.com>
2021-09-02 03:24:18 +0430

    add random button

--- app/src/main/java/com/example/myapplication/DataFactory.kt
+++ app/src/main/java/com/example/myapplication/DataFactory.kt
@@ -20,4 +20,39 @@ object DataFactory {
     )
     val miladEntrance = FarmView.Coordinate(51.365876044, 35.74120467651047)
     val miladOutlet = FarmView.Coordinate(51.375876044, 35.74655967651047)
+
+    val random1Coordinate = listOf(
+        FarmView.Coordinate(51.36135321737392, 35.73178870442762),
+        FarmView.Coordinate(51.37861909692312, 35.7308034894134),
+        FarmView.Coordinate(51.36824002724843, 35.72843100212392),
+        FarmView.Coordinate(51.3713200034581, 35.72176766993188),
+        FarmView.Coordinate(51.38222792809884, 35.72562962654136),
+        FarmView.Coordinate(51.38809342007818, 35.73574893052375),
+        FarmView.Coordinate(51.38215813990917, 35.73575425483868),
+        FarmView.Coordinate(51.38242383492764, 35.74027235018919),
+        FarmView.Coordinate(51.37657770803551, 35.73644196681789),
+        FarmView.Coordinate(51.35926337611597, 35.73610501975674),
+        FarmView.Coordinate(51.36135321737392, 35.73178870442762),
+    )
+    val random2Coordinate = listOf(
+        FarmView.Coordinate(51.36557942093578, 35.71416220989575),
+        FarmView.Coordinate(51.35465785161838, 35.71351100418279),
+        FarmView.Coordinate(51.3547300982766, 35.70783630549784),
+        FarmView.Coordinate(51.34519894737539, 35.70485174279386),
+        FarmView.Coordinate(51.34803072470332, 35.7018682585072),
+        FarmView.Coordinate(51.34355028262055, 35.70130607466601),
+        FarmView.Coordinate(51.35307422975366, 35.69814245497273),
+        FarmView.Coordinate(51.3547239528807, 35.70569492581458),
+        FarmView.Coordinate(51.36289869986608, 35.70821345624451),
+        FarmView.Coordinate(51.37280356248115, 35.71119161513629),
+        FarmView.Coordinate(51.36557942093578, 35.71416220989575)
+    )
+
+    val allCoordinates = listOf(
+        azadiTowerCoordinates,
+        miladTowerCoordinates,
+        random1Coordinate,
+        random2Coordinate
+    )
+
 }
--- app/src/main/java/com/example/myapplication/RootActivity.kt
+++ app/src/main/java/com/example/myapplication/RootActivity.kt
@@ -3,6 +3,7 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.databinding.ActivityRootBinding
+import kotlin.random.Random
 
 class RootActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRootBinding
@@ -10,9 +11,35 @@ class RootActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(ActivityRootBinding.inflate(layoutInflater).also { binding = it }.root)
 
+        binding.btnRandom.setOnClickListener {
+            bindSomeData()
+        }
+
         binding.farm.fieldCoordinates = DataFactory.miladTowerCoordinates
         binding.farm.waterEntrance = DataFactory.miladEntrance
         binding.farm.waterOutlet = DataFactory.miladOutlet
         binding.farm.slopeXY = 20f to 14.4f
+
+    }
+
+    private fun bindSomeData() {
+        val coordinates = DataFactory.allCoordinates.random()
+
+        binding.farm.fieldCoordinates = coordinates
+        binding.farm.waterEntrance = coordinates.random().run {
+            copy(
+                x = x - Random.nextDouble(0.002, 0.004),
+                y = y - Random.nextDouble(0.002, 0.004)
+            )
+        }
+
+        binding.farm.waterOutlet = coordinates.random().run {
+            copy(
+                x = x + Random.nextDouble(0.002, 0.004),
+                y = y + Random.nextDouble(0.002, 0.004)
+            )
+        }
+
+        binding.farm.slopeXY = Random.nextFloat() to Random.nextFloat()
     }
 }
