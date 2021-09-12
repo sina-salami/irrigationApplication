63d2808aa2dd642d4c566bd08cac8267a80d11aa
Farhad Beigirad <f.beigirad@gmail.com>
2021-09-12 23:23:30 +0430

    fix coordination intersection calculations

--- app/src/main/java/ir/sinasalami/irrigation/Extensions.kt
+++ app/src/main/java/ir/sinasalami/irrigation/Extensions.kt
@@ -2,6 +2,7 @@ package ir.sinasalami.irrigation
 
 import android.content.res.Resources
 import org.locationtech.jts.geom.Coordinate
+import kotlin.math.abs
 
 val Float.toPx get() = this * Resources.getSystem().displayMetrics.density
 val Float.toDp get() = this / Resources.getSystem().displayMetrics.density
@@ -9,7 +10,7 @@ val Float.toDp get() = this / Resources.getSystem().displayMetrics.density
 val Int.toPx get() = (this * Resources.getSystem().displayMetrics.density).toInt()
 val Int.toDp get() = (this / Resources.getSystem().displayMetrics.density).toInt()
 
-
-fun Coordinate.isBetween(previous: Coordinate, next: Coordinate): Boolean =
-    x in minOf(previous.x, next.x)..maxOf(previous.x, next.x) &&
-            y in minOf(previous.y, next.y)..maxOf(previous.y, next.y)
+fun Coordinate.isBetween(previous: Coordinate, next: Coordinate): Boolean {
+    val m = (next.y - previous.y) / (next.x - previous.x)
+    return abs((y - previous.y) - (m * (x - previous.x))) < 0.00001
+}
